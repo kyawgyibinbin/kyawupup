faa22eeaad867386197a7596f3a428eb4d175090
kyawgyibinbin <90120798+kyawgyibinbin@users.noreply.github.com>
2022-06-27 11:26:23 +0630

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -3,16 +3,16 @@
 # (c) Shrimadhav U K | Modified By > @DC4_WARRIOR
 
 from pyrogram import filters
-from pyrogram import Client as Clinton
+from pyrogram import Client as Kyawwa
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
 
-@Clinton.on_callback_query(filters.regex('^X0$'))
+@Kyawwa.on_callback_query(filters.regex('^X0$'))
 async def delt(bot, update):
           await update.message.delete(True)
 
 
-@Clinton.on_callback_query()
+@Kyawwa.on_callback_query()
 async def button(bot, update):
 
     cb_data = update.data
